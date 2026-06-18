0dfe59d0ce8d33895746265faa02513e50027f0e
sambelltacq <Sam.Bell@d-tacq.co.uk>
2026-06-18 14:53:54 +0100

    Updated to use new methods 	modified:   acq400_cli/apps/transient_capture.py

--- acq400_cli/apps/transient_capture.py
+++ acq400_cli/apps/transient_capture.py
@@ -5,13 +5,10 @@ Capture transient data on UUTs
 """
 
 import os
-from acq400_cli import ArgTypes, Collection, SigGen, ArgParser, generate_timestamp, plot_by_carrier
+from acq400_cli import ArgTypes, Collection, ArgParser, generate_timestamp
 
 def main(args):
-    
     uuts = Collection(args.uutnames)
-
-    if args.siggen: siggen = SigGen(args.siggen)
 
     uuts.abort_capture()
 
@@ -25,31 +22,29 @@ def main(args):
         translen=args.translen,
     )
 
-    print('Arming')
     uuts.arm_transient()
+    print('Arming')
+
     uuts.wait_for_arm()
     print('Armed')
 
-    if args.trigger.line == 1: uuts.trigger_soft_trigger()
-    elif siggen: siggen.trigger()
+    uuts.trigger_capture(args.siggen)
 
-    uuts.wait_for_idle()
+    uuts.print_status_until_idle()
     print("Complete")
 
     print('Reading data')
     data = uuts.read_transient_data()
-    timestamp = None if args.overwrite else generate_timestamp()
 
+    timestamp = None if args.overwrite else generate_timestamp()
     for uutname, dat in data.items():
-        filename = uuts[uutname].data_filename(dat.sample_format, timestamp)
+        filename = uuts[uutname].data_filename(dat.sample_format.tag, timestamp)
         filepath = os.path.join(args.savedir, filename)
+
         print(f"Saving {uutname} data to {filepath}")
         dat.save_to_file(filepath)
+        
         if args.hexdump: print(f"Hexdump: {dat.sample_format.hexdump} {filepath}")
-
-    if args.pchans:
-        print('Plotting')
-        plot_by_carrier(data, args.pchans)
 
 
 def get_parser():
@@ -62,10 +57,9 @@ def get_parser():
     parser.add_argument('--rgm', default='0,0,0', type=ArgTypes.rgm_triplet, help='RGM triplet')
     parser.add_argument('--translen', default=0, type=int, help='Translen value')
 
-    parser.add_argument('--siggen', default=None, type=int, help='Siggen hostname to EXT trigger')
+    parser.add_argument('--siggen', default=None, type=ArgTypes.siggen, help='Siggen hostname to EXT trigger')
 
     parser.add_argument('--savedir', default="DATA", help='Save dir')
-    parser.add_argument('--pchans', default=None, type=ArgTypes.list_of_channels, help='Channels to plot')
 
     parser.add_argument('--hexdump', action='store_true', help='Print hexdump command')
     parser.add_argument('--overwrite', action='store_true', help='Overwrite data file')
