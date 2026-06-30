19c42fc1927b997d765e5138715a74186107ee80
sambelltacq <Sam.Bell@d-tacq.co.uk>
2026-06-30 15:50:22 +0100

    Added plot_datafile app 	modified:   acq400_cli/apps/plot_datafile.py

--- acq400_cli/apps/plot_datafile.py
+++ acq400_cli/apps/plot_datafile.py
@@ -1,62 +1,91 @@
 #!/usr/bin/env python3
 
 """
-Plot 
+Plot datafiles
 """
 
-import os
-from acq400_cli import ArgTypes, SigGen, ArgParser, plot_by_carrier, parse_filename_parts
+from acq400_cli import ArgTypes, ArgParser
 from acq400_cli.data import UUTData
+from acq400_cli.plotting import FigSpec, Plotter, PCFG
 
 def main(args):
-    
-    for filename in args.filenames:
 
-        parts = parse_filename_parts(filename)
-        #TODO: if egu get calibration from UUT
-        #TODO: if secs and not rate read clock from UUT
-        data = UUTData.from_file(filename, max_samples=args.max_samples)
-        print(data)
-        uutname = parts.hostname or os.path.basename(filename)
-        plot_by_carrier(
-            {uutname: data},
-            chans=args.chans,
-            pses=args.pses,
-            secs=args.secs,
-            rate=args.rate,
-        )
+    sources = [UUTData.from_file(filename) for filename in args.filenames]
+    view = 'VOLTS' if args.egu else 'RAW'
+    units = 'SECONDS' if args.secs else 'SAMPLES'
 
+    if args.pcfg:
+        specs = PCFG.import_pcfg2(args.pcfg)
+    else:
+        specs = []
 
+        if args.plot_by == 'channel':
+            for chan in args.chans:
+                specs.append(FigSpec.from_spec({
+                    'chans': [chan],
+                    'view': view,
+                    'mask': args.mask,
+                }))
 
+            for spad in args.spad:
+                specs.append(FigSpec.from_spec({
+                    'spad': [spad],
+                    'mask': args.mask,
+                }))
 
+        if args.plot_by == 'source':
+            for index in range(len(sources)):
+                specs.append(FigSpec.from_spec({
+                    'chans': args.chans,
+                    'sources': index,
+                    'figure': index,
+                    'view': view,
+                    'mask': args.mask,
+                }))
 
+                if args.spad:
+                    specs.append(FigSpec.from_spec({
+                        'spad': args.spad,
+                        'sources': index,
+                        'figure': index,
+                        'mask': args.mask,
+                    }))
+
+    specs = FigSpec.resolve_relative(specs, max_rows=args.max_rows)
+
+    plt = Plotter(
+        specs=specs,
+        sources=sources,
+        pses=args.pses,
+        units=units,
+        sample_rate=args.rate,
+    )
+    plt.show()
 
 
 
 def get_parser():
-    parser = ArgParser(description='Demux datafiles into channels')
-    parser.add_argument('--tofile', action='store_true', help='Save plot to file')
-    parser.add_argument('--savedir', default='PLOTS', help='Dir to save plot to')
-
+    parser = ArgParser(description='Plot datafiles')
     parser.add_argument('--max_samples', default=None, help='Max samples to load into memory')
-
-    #parser.add_argument('--pspad', default=None, type=ArgTypes.list_of_channels, help='Plot spad Channels (1,2)')
+    parser.add_argument('--max_rows', default=4, type=int, help='Max rows per figure')
 
     parser.add_argument('--egu', action='store_true', help='Plot y axis in volts')
-    #parser.add_argument('--scale', default=10, type=int, help='Max volts for egu')
+    parser.add_argument('--scale', default=None, type=int, help='Set max scale for EGU without calibration')
+
     parser.add_argument('--secs', action='store_true', help='Plot X axis in seconds')
-    parser.add_argument('--rate', default=1000000, type=ArgTypes.int_with_unit, help='Sample rate')
+    parser.add_argument('--rate', '--clock', default=None, type=ArgTypes.int_with_unit, help='Set sample rate for SECS without calibration')
 
     parser.add_argument('--pses', default=(0, None, 1), type=ArgTypes.start_end_stride, help='Plot start:end:stride')
-    parser.add_argument('--chans', default=[1], type=ArgTypes.list_of_channels, help='Channels to plot')# pchans?
+    parser.add_argument('--mask', default=None, help='Mask samples')
 
-    parser.add_argument('--plot_carrier', action='store_true', help='Plot by carrier or file')
-    parser.add_argument('--plot_channel', action='store_true', help='Plot by channel')
+    parser.add_argument('--chans', default=[1], type=ArgTypes.list_of_channels, help='Channels to plot')
+    parser.add_argument('--spad', default=[], type=ArgTypes.list_of_channels, help='Spad Channels to plot')
+
+    parser.add_argument('--plot_by', default='channel', choices=['carrier', 'channel'], help='Plot by source or by channel')
 
     parser.add_argument('--pcfg', default=None, help='Plot config file')
 
-
-    parser.add_argument('filenames', nargs='+', help='data filenames')
+    parser.add_argument('filenames', nargs='+', type=ArgTypes.filepath, help='data filenames')
     return parser
 
 if __name__ == '__main__':
